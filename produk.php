35b64f6e5eaf88a575ba7307aa207162ef9e996f
Raden Ariffudin Rachman <radenariffudin@mail.unpas.ac.id>
2019-10-02 12:32:05 +0700

    Membuat Object Produk

---
+++ produk.php
@@ -0,0 +1,23 @@
+<?php
+
+class Produk {
+    public $judul = "judul", 
+           $penulis = "penulis",
+           $penerbit = "penerbit",
+           $harga = 0;
+}
+
+// $produk1 = new Produk();
+// $produk1->judul = "Naruto";
+// var_dump($produk1);
+
+// $produk2 = new Produk();
+// $produk2->judul = "PES 2019";
+// var_dump($produk2);
+
+$produk3 = new Produk();
+$produk3->judul = "One Piece";
+$produk3->penulis = "Ari";
+$produk3->penerbit = "Antariksa";
+$produk3->harga = 50000;
+var_dump($produk3);
