4ffadff778c4da24bb063c46ec40ad43e407a8fc
Raden Ariffudin Rachman <radenariffudin@mail.unpas.ac.id>
2019-10-04 13:19:30 +0700

    menggunakan autoloading (memanggil semua file yg ada di folder)

---
+++ autoloading/index.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once 'App/init.php';
+
+$produk1 = new Komik("One Piece","Ari","Antariksa", 50000, 100);
+$produk2 = new Game("Dota 2","Lukman","Steam", 1000000, 50);
+
+$cetakProduk = new CetakInfoProduk();
+$cetakProduk->tambahProduk($produk1);
+$cetakProduk->tambahProduk($produk2);
+echo $cetakProduk->cetak();
+
+echo "<hr>";
+
+new Coba();
